f7f74c67630ffdd1f68d7c4e3951cab3552b69ee
Martin <mjm.huber@outlook.de>
2025-11-03 09:38:58 +0100

    mehr beschreibung

--- if.py
+++ if.py
@@ -4,6 +4,6 @@ print("Hello World!")
 a:int=int(sys.argv[1])
 b:int=int(sys.argv[2])
 if b>a :
-    print(b)
+    print("b ist: %d und somit größer als a, welches den Wert %d beträgt"  % (b, a))
 else :
-    print(a)
+    print("a ist: %d und somit größer als b, welches den Wert %d beträgt" % (a, b))
