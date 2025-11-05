36821980108e5e80acd17beb518294850bc898fd
Martin <mjm.huber@outlook.de>
2025-11-05 09:55:54 +0100

    runterzählen

--- Produkt.py
+++ Produkt.py
@@ -1,13 +1,4 @@
 n:int=int(input("Bitte geben Sie eine natürliche Zahl ein:"))
-i:int=int(1)
-p:int=int(1)
-print(i)
-print("*")
-while i<n :
+i:int=int(0)
+for i in range(n, -1, -1) :
     print(i)
-    print("Ergebnis:")
-    p:int=int(p*i)
-    print(p)
-    i:int=int(i+1)
-    if i<n :
-        print("*")
